5200115fc801f5caf9c4a80b78f522e8c2fb44c1
iyosayi-dexter <onaiwuiyosayi@gmail.com>
2023-09-22 20:29:48 +0100

    chore: landing page complete

--- src/assets/components/Footer.tsx
+++ src/assets/components/Footer.tsx
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom';
 
 import styles from 'src/styles/footer.module.scss';
 import IconInstagram from 'src/assets/icons/instagram';
-import IconX from 'src/assets/icons/x'
-import IconFacebook from 'src/assets/icons/facebook'
-import IconLocation from 'src/assets/icons/location'
-import IconLinkedIn from 'src/assets/icons/linkedin'
-import IconPhone from 'src/assets/icons/phone'
+import IconX from 'src/assets/icons/x';
+import IconFacebook from 'src/assets/icons/facebook';
+import IconLocation from 'src/assets/icons/location';
+import IconLinkedIn from 'src/assets/icons/linkedin';
+import IconPhone from 'src/assets/icons/phone';
 
 const Footer = () => {
     return (
@@ -17,7 +17,13 @@ const Footer = () => {
                     <div className={styles.footer__header}>
                         get<span data-color="primary">Linked</span>
                     </div>
-                    <div style={{ lineHeight: '172.4%' }}>
+                    <div
+                        style={{
+                            lineHeight: '172.4%',
+                            marginTop: '0.25rem',
+                            maxWidth: '29rem',
+                        }}
+                    >
                         Getlinked Tech Hackathon is a technology innovation
                         program established by a group of organizations with the
                         aim of showcasing young and talented individuals in the
@@ -25,13 +31,16 @@ const Footer = () => {
                     </div>
 
                     <div className={styles.footer__use}>
-                        <div>Terms of Use </div> <div> Privacy Policy</div>
+                        <Link to="#">Terms of Use </Link>{' '}
+                        <Link to="#"> Privacy Policy</Link>
                     </div>
                 </section>
 
                 {/* --- Two ---  */}
                 <section>
-                    <div className={styles.footer__header}>Useful Links</div>
+                    <div className={styles.footer__listHeader}>
+                        Useful Links
+                    </div>
                     <div className={styles.footer__link}>
                         <Link to="/">Overview</Link>
                     </div>
@@ -44,25 +53,34 @@ const Footer = () => {
                     <div className={styles.footer__link}>
                         <Link to="/">Register</Link>
                     </div>
-                    <div className={styles.footer__link}>
+                    <div className={styles.footer__socials}>
                         <div>Follow us</div>
-                        <IconInstagram/>
-                        <IconX/>
-                        <IconFacebook/>
-                        <IconLinkedIn/>
+                        <div className={styles.footer__icon}>
+                            <IconFacebook />
+                        </div>
+                        <div className={styles.footer__icon}>
+                            <IconInstagram />
+                        </div>
+                        <div className={styles.footer__icon}>
+                            <IconX />
+                        </div>
+                        <div className={styles.footer__icon}>
+                            <IconLinkedIn />
+                        </div>
                     </div>
                 </section>
 
                 {/* --- Three ---  */}
                 <section className={styles.footer__contact}>
-                    <div className={styles.footer__header}><IconPhone/> <span>Contact Us</span></div>
-                    <div className={styles.footer__item}>
-                        <IconLocation/>
+                    <div className={styles.footer__listHeader}>Contact Us</div>
+                    <div className={styles.footer__contact_item}>
+                        <IconPhone />
                         <span>+234 6707653444</span>
                     </div>
 
-                    <div className={styles.footer__item}>
-                        <div>27,Alara Street Yaba 100012 Lagos State</div>
+                    <div className={styles.footer__contact_item}>
+                        <IconLocation />
+                        <span>27,Alara Street Yaba 100012 Lagos State</span>
                     </div>
                 </section>
             </div>
