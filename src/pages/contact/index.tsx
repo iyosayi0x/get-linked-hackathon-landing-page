73d1fd8701f2fa9804ac6712f468467a558c5cfa
iyosayi-dexter <onaiwuiyosayi@gmail.com>
2023-09-23 12:23:19 +0100

    feat: contact form backend

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -7,8 +7,43 @@ import IconInstagram from 'src/assets/icons/instagram';
 import IconX from 'src/assets/icons/x';
 import IconFacebook from 'src/assets/icons/facebook';
 import IconLinkedIn from 'src/assets/icons/linkedin';
+import { BaseSyntheticEvent, useState } from 'react';
+import { handleContact } from 'src/adapters/contact';
 
-const index = () => {
+const Contact = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [form, setForm] = useState<ContactFormArguments>({
+        first_name: '',
+        email: '',
+        message: '',
+        phone_number: '',
+    });
+
+    const handleFormChange = (e: BaseSyntheticEvent) => {
+        setForm((currForm) => ({
+            ...currForm,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    const handleSubmitForm = async (e: BaseSyntheticEvent) => {
+        e.preventDefault();
+        if (
+            !form.email ||
+            !form.message ||
+            !form.phone_number ||
+            !form.first_name
+        )
+            return;
+        try {
+            setIsLoading(true);
+            await handleContact(form);
+        } catch (err) {
+            // do something
+        } finally {
+            setIsLoading(false);
+        }
+    };
     return (
         <div>
             <Header showBorder={false} />
@@ -45,7 +80,10 @@ const index = () => {
                         </div>
                     </section>
                 </aside>
-                <form className={styles.contact__form}>
+                <form
+                    className={styles.contact__form}
+                    onSubmit={handleSubmitForm}
+                >
                     <section className={styles.contact__form_header}>
                         <div>Questions or need assistance?</div>
                         <div>Let us know about it!</div>
@@ -56,6 +94,9 @@ const index = () => {
                             className={formStyles.form__input}
                             type="text"
                             placeholder="Name"
+                            name="first_name"
+                            onChange={handleFormChange}
+                            value={form.first_name}
                         />
                     </div>
 
@@ -64,6 +105,20 @@ const index = () => {
                             className={formStyles.form__input}
                             type="text"
                             placeholder="Mail"
+                            name="email"
+                            onChange={handleFormChange}
+                            value={form.email}
+                        />
+                    </div>
+
+                    <div className={formStyles.form__field}>
+                        <input
+                            className={formStyles.form__input}
+                            type="text"
+                            placeholder="Phone Number"
+                            name="phone_number"
+                            onChange={handleFormChange}
+                            value={form.phone_number}
                         />
                     </div>
 
@@ -72,11 +127,16 @@ const index = () => {
                             className={formStyles.form__input}
                             type="text"
                             placeholder="Message"
+                            name="message"
+                            onChange={handleFormChange}
+                            value={form.message}
                         />
                     </div>
 
                     <div style={{ display: 'grid', placeItems: 'center' }}>
-                        <button>Submit</button>
+                        <button>
+                            {isLoading ? 'Submitting...' : 'Loading...'}
+                        </button>
                     </div>
                 </form>
             </main>
@@ -84,4 +144,4 @@ const index = () => {
     );
 };
 
-export default index;
+export default Contact;
